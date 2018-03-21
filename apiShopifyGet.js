9ec0189f4863df497c02c621fe0ee5c67a81f410
taehyunlim <taehyun.lim@gmail.com>
2018-03-21 16:18:22 -0700

    added a promise object inside api script

--- apiShopifyGet.js
+++ apiShopifyGet.js
@@ -3,7 +3,6 @@
 var request = require('request');
 var fs = require('fs');
 var path = require('path');
-var parse = require('csv-parse');
 var j2c = require('json-2-csv'); // https://www.npmjs.com/package/json-2-csv
 var j2x = require('json2xls'); //https://www.npmjs.com/package/json2xls
 var RateLimiter = require('limiter').RateLimiter;
@@ -21,7 +20,7 @@ var shopname = config.shopify_shopname_dev;
 
 // Database setup
 var mongoose = require('mongoose');
-var models = require('./models/OrderSchema.js')
+// var models = require('./Models/OrderSchema.js')
 var db = mongoose.connection;
 var databaseName = 'zsdb_test';
 var dbURI = 'mongodb://localhost:27017/' + databaseName;
@@ -41,11 +40,35 @@ var lastDocumentNo = '';
 var sysLogFile = 'systemLog.txt';
 var sysLogInitial = '\r\n@' + timestring + ' >>> ' + __filename + '\r\n'
 const systemLog = (log) => {
-    fs.appendFileSync(sysLogFile, sysLogInitial + log);
+	fs.appendFileSync(sysLogFile, sysLogInitial + log);
 }
 
 // Rounding for discount calcuation
 function truncateToCent(value) {
-    return Number(Math.floor(value * 100) / 100);
+	return Number(Math.floor(value * 100) / 100);
 }
 
+var promiseGetOrders = new Promise((resolve, reject) => {
+	request(
+		{
+			url: baseurl + '/admin/orders.json?financial_status=paid&limit=200',
+			json: true,
+		}, function (error, response, body) {
+			if (error) {
+				reject (error);
+			}
+			else {
+				resolve(body);
+			}
+		}
+	)
+})
+
+promiseGetOrders.then((body)=>{
+	console.log("successful");
+	return (body.orders[0]["id"])
+}, (error) => {
+	console.log(error);
+}).then((id)=>{
+	console.log(id);
+})
